e59d1a0f75cd86662ba113ff2cbd9a037e7fcd1b
Sol Vaccaro <vaccaro.marisol93@gmail.com>
2024-02-01 01:21:12 -0300

    Added input

--- src/app/vistas/components/saludo/saludo.component.ts
+++ src/app/vistas/components/saludo/saludo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './saludo.component.css'
 })
 export class SaludoComponent {
+  @Input() mostrarSaludo: boolean = true;
  nombre: string = "Sol"
 }
