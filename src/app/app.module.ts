5bf54f0353d940e3d23371fb6f1e8edbf3cfb898
Sol Vaccaro <vaccaro.marisol93@gmail.com>
2024-02-01 00:40:05 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaludoComponent } from './vistas/components/saludo/saludo.component';
+import { DespedidaComponent } from './vistas/components/despedida/despedida.component';
+import { MainPageComponent } from './vistas/pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaludoComponent,
+    DespedidaComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/vistas/components/despedida/despedida.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-despedida',
+  templateUrl: './despedida.component.html',
+  styleUrl: './despedida.component.css'
+})
+export class DespedidaComponent {
+
+}
