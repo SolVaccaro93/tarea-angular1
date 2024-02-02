821f2477619c90b18190475f9773bed458a06d4e
Sol Vaccaro <vaccaro.marisol93@gmail.com>
2024-02-02 00:38:37 -0300

    changed despedida routing

--- src/app/vistas/components/despedida/despedida.component.ts
+++ src/app/vistas/components/despedida/despedida.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-despedida',
   templateUrl: './despedida.component.html',
   styleUrl: './despedida.component.css'
 })
-export class DespedidaComponent {
-  nombre: string = "Sol";
+
+export class DespedidaComponent implements OnInit {
+  nombre: string = "";
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    this.route.params.subscribe(params => {
+
+      this.nombre = params['nombre'];
+    });
+  }
 }
